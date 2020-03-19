78091461f26b62758b80685a03ad9c744eb84384
defoltem <defolt.mask@gmail.com>
2020-03-19 14:14:01 +0300

    demo v1

--- creatures.h
+++ creatures.h
@@ -14,25 +14,25 @@ protected:
         std::cout << "health point: " << hp << std::endl;
         std::cout << "mana point: " << mp << std::endl;
     }
-    int attack(){
+    int attack(){ // this go first
         return str*0.5;
     }
-    int miss(int dmg){
+    int block(int dmg){ // that thing's going to block input damage
         return dmg-(dex*0.5);
     }
-    void get_dmg(int dmg){
+    void get_dmg(int dmg){ // and, finally, this method hit creature
         hp -= dmg;
     }
 };
 class hero : public creature {
 private:
     std::string name;
+public:
     int lvl;
     int hp;
     int mp;
     int str;
     int dex;
-public:
     hero(int lvl, int hp, int mp, int str, int dex, std::string name):creature(lvl, hp, mp, str, dex), name(name){
         this->lvl = lvl;
         this->hp = hp;
@@ -45,18 +45,56 @@ public:
         std::cout << "level: " << lvl << std::endl;
         std::cout << "health point: " << hp << std::endl;
         std::cout << "mana point: " << mp << std::endl;
-
+        std::cout << "strength: " << str << std::endl;
+        std::cout << "dexterity: " << dex << std::endl;                
     }
     int attack() {
         return str*0.5;
     }
-    int miss(int dmg) {
+    int block(int dmg) {
+        return dmg-(dex*0.5);
+    }
+    int get_dmg(int dmg) {
+        return hp -= dmg;
+    }
+    void the_fuck(){
+        std::cout << "fuck" << '\n';
+    }
+};
+class enemy : public creature {
+private:
+    std::string name;
+    int lvl;
+    int mp;
+    int str;
+    int dex;
+public:
+    int hp;
+    enemy(int lvl, int hp, int mp, int str, int dex) : creature(lvl, hp, mp, str, dex){
+        this->lvl = lvl;
+        this->hp = hp;
+        this->mp = mp;
+        this->str = str;
+        this->dex = dex;
+        if (lvl <= 5) name = "goblin";
+        else if (lvl <= 10) name = "golem";
+        else name = "slave of eternity";
+    }
+    void status() {
+        std::cout << "named: " << name << std::endl;
+        std::cout << "level: " << lvl << std::endl;
+        std::cout << "health point: " << hp << std::endl;
+        std::cout << "mana point: " << mp << std::endl;
+        std::cout << "strength: " << str << std::endl;
+        std::cout << "dexterity: " << dex << std::endl;                
+    }
+    int attack() {
+        return str*0.5;
+    }
+    int block(int dmg) {
         return dmg-(dex*0.5);
     }
     void get_dmg(int dmg) {
         hp -= dmg;
-    }
-    void the_fuck(){
-        std::cout << "fuck" << '\n';
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,9 +1,73 @@
 #include <iostream>
 #include "creatures.h"
+#include <string>
+#include <ncurses.h>
+
+void line(){
+    std::cout << "----------------\n";
+}
+void all_status(hero h, enemy e){
+    h.status();
+    line();
+    e.status();    
+}
 
 int main(){
-    hero hr(1, 10, 10, 10, 10,"ku"); // creture(lvl, hp, mp, str, dex), name(name) 
-    std::cout<< hr.attack() <<'\n';
+    bool hero_turn = true;
+    const int size_arr = 4;
+    short int for_cicle = size_arr;
+    int arrn[size_arr] = {5, 5, 5, 5};
+    std::string arrs[size_arr] = {"hp", "mp", "str", "dex"}; // just for creating hero
+    short int points = 3, step = 0;
+    std::string n;
+    std::cout << "write your name: ";
+    getline(std::cin, n);
+    const std::string cn = n; // creat hero's name
+    std::cout << cn << '\n';
+    std::cout << "each your characteristic is equal to 5 and you have only 3 points\nif you don't want to add points then just write zero\n";
+    while(points > 0){
+        int per;
+        std::cout << "you have " << points << " points\n";
+        for(int i = 0; i < for_cicle; i++){
+            std::cout << arrs[i] << ": " << arrn[i] << '\n';
+            //points = 0;  
+        }
+        std::cout << "add to " << arrs[step] << "? ";
+        std::cin>>per;
+        if(per > points){
+            std::cout << "your value's more then avaliable points\n";
+            for_cicle++;
+            continue;
+        }
+        points -= per;
+        arrn[step] += per;
+        step++;
+        line();
+    }
+    hero hr(1, arrn[0], arrn[1], arrn[2], arrn[3], cn); // creture(lvl, hp, mp, str, dex), name(name) 
     hr.status();
+    while(1){
+        enemy e(hr.lvl, hr.hp, hr.mp, hr.str, hr.dex);
+        while(e.hp > 0 || hr.hp > 0){
+            if(hero_turn) {
+                e.get_dmg(hr.attack());
+                hero_turn = false;
+                }
+            else {
+                hr.get_dmg(e.attack());
+                hero_turn = true;
+            }
+            all_status(hr, e);
+        }
+        break;
+    }
+    /*std::cout<< hr.attack() <<'\n';
+    enemy e(hr.lvl, hr.hp, hr.mp, hr.str, hr.dex);
+    e.status();
+    std::cout << "-------------\n";
+    hr.status();
+    std::cout << "goblin`s attack: " << e.attack() << '\n';
+    e.get_dmg(8); 
+    e.status();*/
     return 0;
 }
